13b14fec693a0fd6dc8a4c57c78ff6052f892927
TodorAtanassov <todoratanasov432@gmail.com>
2022-05-26 16:15:34 +0300

    Linear_regression_v2

--- Linear regression.py
+++ Linear regression.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression as lr
+from sklearn.model_selection import train_test_split
 t_s = np.array([20, 50, 32, 65, 23, 43, 10, 5, 22, 35, 29, 5, 56]).reshape(-1, 1)
 score = np.array([56, 83, 47, 93, 47, 82, 45, 23, 55, 67, 57, 4, 89]).reshape(-1, 1)
 
-model = lr()
-model.fit(t_s, score)
+time_train, time_test, score_train, score_test = train_test_split(t_s, score, test_size=0.3)
 
-print(model.predict(np.array([34]).reshape(-1, 1)))
-plt.scatter(t_s, score)
+model = lr()
+model.fit(time_train, score_train)
+
+print(model.score(time_test, score_test))
+plt.scatter(time_train, score_train)
 plt.plot(np.linspace(0, 70, 100).reshape(-1, 1), model.predict(np.linspace(0, 70, 100).reshape(-1, 1)), 'r')
 plt.ylim(0, 100)
 plt.show()
